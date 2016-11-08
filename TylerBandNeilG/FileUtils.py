81d3bb785087df413e79465d1630894ad522c2f4
Tyler Black <blac0403@algonquinlive.com>
2016-11-08 16:07:55 -0500

    first three questions

--- TylerBandNeilG/FileUtils.py
+++ TylerBandNeilG/FileUtils.py
@@ -16,6 +16,7 @@ _scriptFolder = os.path.dirname(os.path.abspath(__file__))
 def main():
     test_writeToFile()
     test_getFileContent()
+    test_appendToFile()
 
 def writeToFile(fileName, content):
     outfile = open(fileName, 'w')
@@ -31,9 +32,17 @@ def getFileContent(fileName):
     except IOError:
         return fileName + " does not exist."
 
+def appendToFile(fileName, content):
+    if os.path.exists(fileName):
+        outFile = open(fileName, 'a')
+        outFile.write(content)
+        outFile.close()
+    else:
+        print fileName + " does not exist."
 
 def test_writeToFile():
     expected = "Some junk text."
+    writeToFile("fileDemo.txt", expected)
     func = 'writeToFile'
     fileName = os.path.join(_scriptFolder,"fileDemo.txt")
     inText = open(fileName)
@@ -58,6 +67,28 @@ def test_getFileContent():
     actual = getFileContent(os.path.join(_scriptFolder,"fileDem.txt"))
     if expected == actual:
         print _fmtPassed.format("getFileContent(fileName)")
+    else:
+        print _fmtFailed.format(func, expected, actual)
+
+def test_appendToFile():
+    expected = "Some junk text.More junk text."
+    appendToFile("fileDemo.txt", "More junk text.")
+    func = 'appendToFile'
+    fileName = os.path.join(_scriptFolder,"fileDemo.txt")
+    inText = open(fileName)
+    actual = inText.read()
+    if expected == actual:
+        print _fmtPassed.format("appendToFile(fileName, content)")
+    else:
+        print _fmtFailed.format(func, expected, actual)
+#When file doesn't exist
+    expected = None
+    func = 'appendToFile'
+##    fileName = os.path.join(_scriptFolder,"fileDemo.txt")
+##    inText = open(fileName)
+    actual = appendToFile("fileDe.txt", "More junk text.")
+    if expected == actual:
+        print _fmtPassed.format("appendToFile(fileName, content)")
     else:
         print _fmtFailed.format(func, expected, actual)
 
