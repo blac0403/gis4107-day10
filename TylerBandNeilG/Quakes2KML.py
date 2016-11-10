991684d5d72a05ead9057f16d94789311d183a55
Neil Gilchrist <neil.g.gilchrist@gmail.com>
2016-11-10 12:46:17 -0500

    kml func

---
+++ TylerBandNeilG/Quakes2KML.py
@@ -0,0 +1,64 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Neil
+#
+# Created:     10-11-2016
+# Copyright:   (c) Neil 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import os
+import sys
+
+_fmtPassed = "Passed: {}"
+_fmtFailed = "Failed: {}\n  Expect: {}\n  Actual: {}"
+_scriptFolder = os.path.dirname(os.path.abspath(__file__))
+
+def main():
+    test_getKmlPlacemark()
+
+def exportToKml(inFile, outKml):
+    for inText in open(inFile):
+        list(inText)
+        inText.split(',')
+
+        lat = inText[1]
+        lon = inText[2]
+        depth = inText[3]
+        mag = inText[4]
+
+        header = getKmlHeader()
+        body = getKmlPlacemark(lon,lat,depth,mag)
+        footer = getKmlFooter()
+        return header,body,footer
+
+def getKmlHeader():
+    return " <?xml version=1.0 encoding=UTF-8?> \n <kml xmlns=http://www.opengis.net/kml/2.2>"
+
+
+def getKmlPlacemark(longitude, latitude, depth, magnitude):
+    return " <Placemark> \n  <name> %s </name> \n  <description> Mag=%s, Depth =%s km  </description> \n  <Point> \n   <coordinates>%s,%s</coordinates> \n  </Point>\n  </Placement>" % (magnitude,magnitude,depth,longitude,latitude)
+
+def getKmlFooter():
+    return "</Document> \n </kml>"
+
+def test_getKmlPlacemark():
+    print getKmlPlacemark(-116.355, 34.692, 12.3, 1.21)
+
+if __name__ == '__main__':
+    main()
+
+
+##
+### Test case 1
+##    expected = "expected value"
+##    func = "func1"
+##    actual = func1(1)
+##    if expected == actual:
+##        print _fmtPassed.format("func1(params)")
+##    else:
+##        print _fmtFailed.format(func, expected, actual)
+##
+##    # Test case 2 ...
