11b0be5469c861f87e8d04f3db2fa35780caba06
Neil Gilchrist <neil.g.gilchrist@gmail.com>
2016-11-12 16:34:44 -0500

    basically done, just need the testing

--- TylerBandNeilG/Quakes2KML.py
+++ TylerBandNeilG/Quakes2KML.py
@@ -17,12 +17,12 @@ _fmtFailed = "Failed: {}\n  Expect: {}\n  Actual: {}"
 _scriptFolder = os.path.dirname(os.path.abspath(__file__))
 
 def main():
-    #test_getKmlPlacemark()
+    test_getKmlPlacemark()
     test_exportToKml()
 
 def exportToKml(inFile, outKml):
     outFile = open(outKml, 'w')
-
+    counter = 0
     inText = open(inFile, 'r')
 
     for line in inText:
@@ -31,30 +31,34 @@ def exportToKml(inFile, outKml):
         lon = kmlList[2]
         depth = kmlList[3]
         mag = kmlList[4]
-
+        if lat == 'Lat': continue
         header = getKmlHeader()
         body = getKmlPlacemark(lon,lat,depth,mag)
         footer = getKmlFooter()
 
         outFile.write(header +body +footer)
 
-    print kmlList
-
     outFile.close()
 
 
 def getKmlHeader():
-    return " <?xml version=1.0 encoding=UTF-8?> \n <kml xmlns=http://www.opengis.net/kml/2.2>"
+    return "<?xml version=1.0 encoding=UTF-8?> \n<kml xmlns=http://www.opengis.net/kml/2.2> \n<Document>\n"
 
 
 def getKmlPlacemark(longitude, latitude, depth, magnitude):
-    return " <Placemark> \n  <name> %s </name> \n  <description> Mag=%s Depth =%s km  </description> \n  <Point> \n   <coordinates>%s,%s</coordinates> \n  </Point>\n  </Placement>" % (magnitude,magnitude,depth,longitude,latitude)
-
+    #Formatting not working as intented - after magntidue variable it creates a new line when printing to a text document, but works in interpreter
+    return """    <Placemark>
+      <name>%s</name>
+      <description>Mag=%s, Depth=%s km</description>
+      <Point>
+        <coordinates>%s,%s</coordinates>
+      </Point>
+    </Placemark>""" % (magnitude,magnitude,depth,longitude,latitude)
 def getKmlFooter():
-    return "</Document> \n </kml>"
+    return "</Document> \n </kml> \n"
 
-##def test_getKmlPlacemark():
-##    print getKmlPlacemark(-116.355, 34.692, 12.3, 1.21)
+def test_getKmlPlacemark():
+    print getKmlPlacemark(-116.355, 34.692, 12.3, 1.21)
 
 def test_exportToKml():
     exportToKml(r"C:\acgis\gis4107\day10\lab\TylerBandNeilG\TylerBandNeilG\quakes2000\quakes2000.txt", "Kml_quakes2000.txt")
